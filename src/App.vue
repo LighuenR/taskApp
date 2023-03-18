5b1bcaa19b177f63055f66844bad835ff86dab33
Lighuen Restanio <82963034+LighuenR@users.noreply.github.com>
2023-03-18 21:33:13 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <app-header></app-header>
+    <v-main>
+    
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue"
+
+export default {
+  name: 'App',
+
+  components: {
+    AppHeader,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
