eb2af8d1503267c2f10a79f772d2e0517f78d2b1
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-03-18 23:26:44 +0100

    Navigation drawer added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,15 @@
 <template>
     <v-app id="inspire">
-      <v-navigation-drawer v-model="drawer">
-        <!--  -->
+        <v-navigation-drawer
+        v-model="drawer"
+        app
+        theme="dark"
+      >
+        <v-list  nav>
+          <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
+          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+        </v-list>
       </v-navigation-drawer>
   
       <v-app-bar>
