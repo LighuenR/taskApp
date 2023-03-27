64df0abdf5d4653de585399279ba5042e0a6cb84
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-03-27 21:32:05 +0200

    Appbar Image added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,37 +1,38 @@
 <template>
-    <v-app id="inspire">
-        <v-navigation-drawer
-        v-model="drawer"
-        app
-        theme="dark"
-      >
-        <v-list  nav>
-          <v-container>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app theme="dark">
+      <v-list nav>
+        <v-container>
           <v-list-item-title>Vuetify Todo</v-list-item-title>
           <v-list-item-subtitle>Not ready</v-list-item-subtitle>
-          </v-container>
-     
-          <v-list-item to="/" prepend-icon="mdi-view-dashboard" title="Home" value="Indox"></v-list-item>
-          <v-list-item to="/about" prepend-icon="mdi-account-supervisor-circle" title="About" value="supervisors"></v-list-item>
-          <v-list-item to="/todo" prepend-icon="mdi-format-list-checks" title="Todo" value="supervisors"></v-list-item>
+        </v-container>
 
-        </v-list>
-      </v-navigation-drawer>
+        <v-list-item to="/" prepend-icon="mdi-view-dashboard" title="Home" value="Indox"></v-list-item>
+        <v-list-item to="/about" prepend-icon="mdi-account-supervisor-circle" title="About"
+          value="supervisors"></v-list-item>
+        <v-list-item to="/todo" prepend-icon="mdi-format-list-checks" title="Todo" value="supervisors"></v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-app-bar app color="teal-darken-4" image="https://picsum.photos/1920/">
+
+    
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
+
+    </v-app-bar>
+
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
   
-      <v-app-bar>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-  
-        <v-toolbar-title>Aplication</v-toolbar-title>
-      </v-app-bar>
-  
-      <v-main>
-       <router-view></router-view>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script>
-    export default {
-      data: () => ({ drawer: null }),
-    }
-  </script>
+<script>
+export default {
+  data: () => ({ drawer: null }),
+}
+</script>
