3a892b617c85af30876b111b2285c9420d40e458
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-03-28 20:27:59 +0200

    List methods ready

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,44 +1,63 @@
 <template>
-
-        <v-card>
-            <v-list select-strategy="classic">
-                <div v-for="task in tasks" :key="task.id">
-                    <v-list-item >
-                        <template v-slot:prepend="{ isActive }">
-                            <v-list-item-action start>
-                                <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-                            </v-list-item-action>
-                        </template>
-                        <v-list-item-title>{{ task.title }}</v-list-item-title>
-                    </v-list-item>
-                    <v-divider></v-divider>
-                </div>
-            </v-list>
-        </v-card>
- 
+    <v-card>
+        <v-list select-strategy="classic" class="pa-0">
+            <div v-for="task in tasks" :key="task.id">
+                <v-list-item @click="doneTask(task.id)" :class="{ 'blue': task.done }">
+                    <template v-slot:prepend>
+                        <v-list-item-action start>
+                            <v-checkbox-btn :model-value="task.done"></v-checkbox-btn>
+                        </v-list-item-action>
+                    </template>
+                    <v-list-item-title
+                   :class="{ ' text-decoration-line-through': task.done }"
+                    >
+                        {{ task.title }}
+                    </v-list-item-title>
+                </v-list-item>
+                <v-divider></v-divider>
+            </div>
+        </v-list>
+    </v-card>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
             tasks: [
                 {
                     id: 1,
-                    title: 'Wake up'
+                    title: 'Wake up',
+                    done: false
                 },
                 {
                     id: 2,
-                    title: 'Study vue and vuetify'
+                    title: 'Study vue and vuetify',
+                    done: false
                 },
                 {
                     id: 3,
-                    title: 'Clean the house'
-                },
+                    title: 'Clean the house',
+                    done: false
+                }
             ]
+        }
+
+
+    },
+    methods: {
+
+        doneTask(id) {
+            let task = this.tasks.filter(task => task.id === id)[0]
+            task.done = !task.done
         }
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.blue {
+    background: rgb(195, 195, 195)
+}
+</style>
